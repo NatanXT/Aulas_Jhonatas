2178e39f194e8a85927c99f58adc0afa99b84d12
Natan Derick <114193508+NatanXT@users.noreply.github.com>
2024-08-21 14:23:35 -0300

    Primeira
    Minha primeira atividade

---
+++ primeiraAula/src/main/java/com/aulas/primeiraAula/PrimeiraAulaApplication.java
@@ -0,0 +1,15 @@
+package com.aulas.primeiraAula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraAulaApplication {
+
+	public static void main(String[] args) {
+
+
+
+	}
+}
+
---
+++ primeiraAula/src/test/java/com/aulas/primeiraAula/PrimeiraAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulas.primeiraAula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
